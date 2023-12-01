66b2e89678ef48efd3cb98f14ea776c9d8551b9b
Jamin Thornsberry <jamin.thorns@gmail.com>
2023-12-01 17:22:16 -0600

    Improve day 1 solution
    This extracts digits using binary pattern matching.

--- lib/day_1.ex
+++ lib/day_1.ex
@@ -1,13 +1,6 @@
 defmodule Day1 do
   @behaviour Solution
 
-  @digits 1..9
-  @char_digits Enum.map(@digits, &to_string/1)
-  @word_digits ~w(one two three four five six seven eight nine)
-
-  @char_digit_mapping @char_digits |> Enum.zip(@digits) |> Map.new()
-  @word_digit_mapping @word_digits |> Enum.zip(@digits) |> Map.new()
-
   @test_input_1 """
   1abc2
   pqr3stu8vwx
@@ -20,7 +13,7 @@ defmodule Day1 do
   142
   """
   def solve_part_1(input) do
-    calibrate(input, @char_digit_mapping)
+    calibrate(input, &extract_digit/1)
   end
 
   @test_input_2 """
@@ -38,34 +31,51 @@ defmodule Day1 do
   281
   """
   def solve_part_2(input) do
-    calibrate(input, Map.merge(@char_digit_mapping, @word_digit_mapping))
+    calibrate(input, &extract_digit_or_word/1)
   end
 
-  defp calibrate(input, digit_mapping) do
+  defp calibrate(input, extract) do
     input
     |> String.split()
-    |> Enum.map(&digits(&1, digit_mapping))
+    |> Enum.map(&digits(&1, extract))
     |> Enum.map(&Integer.undigits([List.first(&1), List.last(&1)]))
     |> Enum.sum()
   end
 
-  defp digits(line, digit_mapping) do
-    representations = Map.keys(digit_mapping)
-
+  defp digits(line, extract) do
     Stream.iterate({[], line}, fn {digits, line} ->
-      case Enum.find(representations, &String.starts_with?(line, &1)) do
-        nil ->
-          {digits, String.slice(line, 1..-1//1)}
-
-        representation ->
-          digits = digits ++ [Map.get(digit_mapping, representation)]
-          line = String.replace_prefix(line, representation, "")
-
-          {digits, line}
+      case extract.(line) do
+        {nil, rest} -> {digits, rest}
+        {digit, rest} -> {digits ++ [digit], rest}
       end
     end)
-    |> Stream.drop_while(fn {_digits, line} -> line != "" end)
-    |> Stream.map(fn {digits, _line} -> digits end)
-    |> Enum.at(0)
+    |> Enum.find(fn {_digits, line} -> line == "" end)
+    |> elem(0)
+  end
+
+  def extract_digit("1" <> rest), do: {1, rest}
+  def extract_digit("2" <> rest), do: {2, rest}
+  def extract_digit("3" <> rest), do: {3, rest}
+  def extract_digit("4" <> rest), do: {4, rest}
+  def extract_digit("5" <> rest), do: {5, rest}
+  def extract_digit("6" <> rest), do: {6, rest}
+  def extract_digit("7" <> rest), do: {7, rest}
+  def extract_digit("8" <> rest), do: {8, rest}
+  def extract_digit("9" <> rest), do: {9, rest}
+  def extract_digit(<<_first>> <> rest), do: {nil, rest}
+
+  def extract_word("one" <> rest), do: {1, rest}
+  def extract_word("two" <> rest), do: {2, rest}
+  def extract_word("three" <> rest), do: {3, rest}
+  def extract_word("four" <> rest), do: {4, rest}
+  def extract_word("five" <> rest), do: {5, rest}
+  def extract_word("six" <> rest), do: {6, rest}
+  def extract_word("seven" <> rest), do: {7, rest}
+  def extract_word("eight" <> rest), do: {8, rest}
+  def extract_word("nine" <> rest), do: {9, rest}
+  def extract_word(<<_first>> <> rest), do: {nil, rest}
+
+  def extract_digit_or_word(line) do
+    with {nil, _rest} <- extract_digit(line), do: extract_word(line)
   end
 end
